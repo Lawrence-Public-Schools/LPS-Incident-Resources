31cfd2ccb80d2eaa4d7d213a990f94aa3cf4a60c
Logan-Arias-LPS <71857092+Logan-Arias-LPS@users.noreply.github.com>
2020-10-13 20:16:56 -0400

    Updated to v1.3

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @echo off
-git archive --format zip --output dists\LPS-Discipline-Resources-1.0.zip --worktree-attributes --verbose -9 HEAD
+git archive --format zip --output dists\LPS-Discipline-Resources-1.3.zip --worktree-attributes --verbose -9 HEAD
 pause
