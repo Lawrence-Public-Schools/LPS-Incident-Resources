eaface011a503a10ea159f198978bb89378071cb
Logan-Arias-LPS <71857092+Logan-Arias-LPS@users.noreply.github.com>
2021-10-22 17:41:15 -0400

    version 1.7
    version 1.7

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @echo off
-git archive --format zip --output dists\LPS-Discipline-Resources-1.6.zip --worktree-attributes --verbose -9 HEAD
+git archive --format zip --output dists\LPS-Discipline-Resources-1.7.zip --worktree-attributes --verbose -9 HEAD
 pause
